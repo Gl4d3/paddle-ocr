b47179f0b09666a9678fc45e855ef2f71b892799
Anthony Kioko <akioko367@gmail.com>
2024-07-22 22:44:51 +0300

    Kumalo basics

---
+++ basic-paddle/test-paddleocr.py
@@ -0,0 +1,19 @@
+from paddleocr import PaddleOCR
+
+# Initialize PaddleOCR
+ocr = PaddleOCR(use_angle_cls=True)
+
+print("PaddleOCR initialized successfully!")
+
+# Run PaddleOCR on an image
+img_path = 'test.jpg'
+result = ocr.ocr(img_path, cls=True)
+
+# Print the result
+# print(result)       
+table_data = []
+for line in result:
+    table_data.append([line[1][0], f"{line[1][1]:.4f}", str(line[0])])
+
+headers = ["Text", "Confidence", "Position"]
+print(tabulate(table_data, headers=headers, tablefmt="grid"))
