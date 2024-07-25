ac2e474997e008447a8927278a98b4ee8b1e620a
Anthony Kioko <akioko367@gmail.com>
2024-07-25 14:18:55 +0300

    Cleaning up some random stuff

--- tenth-meter-digits/model-training.py
+++ tenth-meter-digits/model-training.py
@@ -1,92 +1,136 @@
-# To train the model on the dataset, run the following code:
-
-# CELL 3
-
 import numpy as np
-
-import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
 import tensorflow as tf
+from sklearn.model_selection import train_test_split
 from tensorflow import keras
+import matplotlib.pyplot as plt
 
-num_samples = 100  # Replace with the actual number of samples
+def train_model():
+    """
+    Trains a model on a dataset of meter digits.
 
-x_data = np.zeros((num_samples, 32, 20, 3))
-y_data = np.zeros((num_samples,))
-y_file = np.zeros((num_samples,))
+    This code trains a machine learning model to recognize meter digits using a dataset of images.
+    It follows the following steps:
+    1. Prepare the dataset by defining the input data (x_data), target labels (y_data), and file names (y_file).
+    2. Split the dataset into training and testing sets.
+    3. Normalize the input data.
+    4. Build a neural network model with two dense layers.
+    5. Compile the model with an optimizer, loss function, and metrics.
+    6. Train the model on the training data.
+    7. Evaluate the model on the testing data.
+    8. Save the trained model.
+    9. Load the saved model.
+    10. Make predictions using the loaded model.
+    11. Plot the first 10 test images, their predicted labels, and the true labels.
+    12. Save the model as a .h5 file.
 
-test_image = # Replace with the actual image data
-category = # Replace with the actual category
-base = # Replace with the actual base value
+    Returns:
+    - None
+    """
+    # Define x_data, y_data, and y_file
+    x_data = np.zeros((10, 32, 20, 3))
+    y_data = np.zeros(10)
+    y_file = np.zeros(10)
 
-for i in range(num_samples):
+    # CELL 3
+
+    test_image = # define test_image variable here
+    i = # define i variable here
+    category = # define category variable here
+    base = # define base variable here
+
     x_data[i] = np.array(test_image)
     y_data[i] = category
     y_file[i] = base
 
-# Split the data into training and testing
-x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size=0.2, random_state=42)
+    # CELL 4
 
-# Normalize the data
-x_train = x_train / 255.0
-x_test = x_test / 255.0
+    # Split the data into training and testing
+    x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size=0.2, random_state=42)
 
-# Build the model
-model = keras.Sequential([
-    keras.layers.Flatten(input_shape=(32, 20, 3)),
-    keras.layers.Dense(128, activation='relu'),
-    keras.layers.Dense(10)
-])
+    # CELL 5
 
-model.compile(optimizer='adam',
-              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-              metrics=['accuracy'])
+    # Normalize the data
+    x_train = x_train / 255.0
+    x_test = x_test / 255.0
 
-# Train the model
-model.fit(x_train, y_train, epochs=10)
+    # CELL 6
 
-# Evaluate the model
-test_loss, test_acc = model.evaluate(x_test,  y_test, verbose=2)
-print('\nTest accuracy:', test_acc)
+    # Build the model
+    model = keras.Sequential([
+        keras.layers.Flatten(input_shape=(32, 20, 3)),
+        keras.layers.Dense(128, activation='relu'),
+        keras.layers.Dense(10)
+    ])
 
-# Save the model
-model.save('meter_digits_model')
+    model.compile(optimizer='adam',
+                  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                  metrics=['accuracy'])
 
-# Load the model
-new_model = keras.models.load_model('meter_digits_model')
+    # CELL 7
 
-# Make predictions
-probability_model = tf.keras.Sequential([new_model,
-                                        tf.keras.layers.Softmax()])
+    # Train the model
+    model.fit(x_train, y_train, epochs=10)
 
-predictions = probability_model.predict(x_test)
+    # CELL 8
 
-# Plot the first 10 test images, their predicted label, and the true label
-# Color correct predictions in blue, incorrect predictions in red
-fig = plt.figure(figsize=(20, 20))
-for i in range(10):
-    fig.add_subplot(5, 5, i+1)
-    plt.imshow(x_test[i], cmap=plt.cm.binary)
-    plt.xticks([])
-    plt.yticks([])
-    predicted_label = np.argmax(predictions[i])
-    true_label = y_test[i]
-    if predicted_label == true_label:
-        color = 'blue'
-    else:
-        color = 'red'
-    plt.xlabel("{} ({})".format(predicted_label, true_label), color=color)
+    # Evaluate the model
+    test_loss, test_acc = model.evaluate(x_test,  y_test, verbose=2)
+    print('\nTest accuracy:', test_acc)
 
-plt.show()
+    # Expected output
+    # Test accuracy: 0.0
 
-# Expected output
-# A plot showing the first 10 test images, their predicted label, and the true label
+    # CELL 9
 
-# CELL 13
+    # Save the model
+    model.save('meter_digits_model')
 
-# Save the model as a .h5 file
-new_model.save('meter_digits_model.h5')
+    # Expected output
+    # INFO:tensorflow:Assets written to: meter_digits_model/assets
 
-# Expected output
-# INFO:tensorflow:Assets written to: meter_digits_model.h5/assets
+    # CELL 10
+
+    # Load the model
+    new_model = keras.models.load_model('meter_digits_model')
+
+    # CELL 11
+
+    # Make predictions
+    probability_model = tf.keras.Sequential([new_model,
+                                            tf.keras.layers.Softmax()])
+
+    predictions = probability_model.predict(x_test)
+
+    # CELL 12
+
+    # Plot the first 10 test images, their predicted label, and the true label
+    # Color correct predictions in blue, incorrect predictions in red
+
+    fig = plt.figure(figsize=(20, 20))
+    for i in range(10):
+        fig.add_subplot(5, 5, i+1)
+        plt.imshow(x_test[i], cmap=plt.cm.binary)
+        plt.xticks([])
+        plt.yticks([])
+        predicted_label = np.argmax(predictions[i])
+        true_label = y_test[i]
+        if predicted_label == true_label:
+            color = 'blue'
+        else:
+            color = 'red'
+        plt.xlabel("{} ({})".format(predicted_label, true_label), color=color)
+
+    plt.show()
+
+    # Expected output
+    # A plot showing the first 10 test images, their predicted label, and the true label
+
+    # CELL 13
+
+    # Save the model as a .h5 file
+    new_model.save('meter_digits_model.h5')
+
+    # Expected output
+    # INFO:tensorflow:Assets written to: meter_digits_model.h5/assets
+
+train_model()
