1d224c58deb4e4a094093b423fdd0d3cfff4f81f
Anthony Kioko <akioko367@gmail.com>
2024-07-27 08:24:18 +0300

    A little change

--- basic-paddle/test-paddleocr.py
+++ basic-paddle/test-paddleocr.py
@@ -12,6 +12,7 @@ result = ocr.ocr(img_path, cls=True)
 # Print the result
 # print(result)       
 table_data = []
+table_data.append([line[1][0], f"{line[1][1][0]:.4f}, {line[1][1][1]:.4f}", str(line[0])])
 for line in result:
     table_data.append([line[1][0], f"{line[1][1]:.4f}", str(line[0])])
 
