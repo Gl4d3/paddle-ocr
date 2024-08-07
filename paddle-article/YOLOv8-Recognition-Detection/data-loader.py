8aabc9a41cc3e045c80633472abc5610ad17d089
Gl4d3 <akioko367@gmail.com>
2024-08-07 17:55:51 +0300

    Added hosting website

--- paddle-article/YOLOv8-Recognition-Detection/data-loader.py
+++ paddle-article/YOLOv8-Recognition-Detection/data-loader.py
@@ -4,3 +4,6 @@ rf = Roboflow(api_key="MM2rTKK4OnFjzDPdpQBe")
 project = rf.workspace("meter-i5vud").project("meter-digits-frvqn")
 version = project.version(2)
 dataset = version.download("yolov8")
+
+# SITE hosted
+# https://universe.roboflow.com/meter-i5vud/meter-digits-frvqn/model/2
